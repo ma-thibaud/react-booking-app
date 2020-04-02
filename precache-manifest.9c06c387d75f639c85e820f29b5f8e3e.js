e4c4d27e77f34e3568fb6ca86aeb09eba7939db0
Mathias Thibaud <math.thibaud@gmail.com>
2020-04-02 16:09:40 -0700

    Updates

---
+++ precache-manifest.9c06c387d75f639c85e820f29b5f8e3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1fcf79333f89f02e3cf4b25f776be4c",
+    "url": "/react-booking-app/index.html"
+  },
+  {
+    "revision": "319950da1be33912f4d8",
+    "url": "/react-booking-app/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "d2aa8268302ea3d9c3f4",
+    "url": "/react-booking-app/static/css/main.963435ea.chunk.css"
+  },
+  {
+    "revision": "319950da1be33912f4d8",
+    "url": "/react-booking-app/static/js/2.69faa486.chunk.js"
+  },
+  {
+    "revision": "e386ada3ad03a9606684b1f649731b40",
+    "url": "/react-booking-app/static/js/2.69faa486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2aa8268302ea3d9c3f4",
+    "url": "/react-booking-app/static/js/main.486e55f9.chunk.js"
+  },
+  {
+    "revision": "67c5e3ff378d935b8baa",
+    "url": "/react-booking-app/static/js/runtime-main.c14f4b7a.js"
+  }
+]);
